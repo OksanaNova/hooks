bc068b0f0705ac61691ec1086a3d94ccc6c4288c
OksanaNova <o.nova0804@gmail.com>
2025-01-29 09:42:21 -0700

    Focus.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 
-import { useState, useRef } from 'react';
 import './App.css'
+import Focus from './Focus';
 
 function App() {
 
-  const [city, setCity] = useState("");
-  const inputRef = useRef(null);
-
-  const focus = () => {
-    inputRef.current.focus()
-  }
-
   return (
     <>
-      <input ref={inputRef} value={city} onChange={e => setCity(e.target.value)} />
-      <p>I want to go to {city}</p>
-      <button onClick={focus}>Use Ref</button>
+
+    <Focus />
+
 
     </>
   )
